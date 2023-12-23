3b16cb37ca96bddf0946e08f0b67b22beff20eca
Michell Pineda <michellpine@hotmail.com>
2023-12-22 22:58:39 -0500

    Uploading React progress!

---
+++ src/components/MainGoal.jsx
@@ -0,0 +1,7 @@
+function MainGoal() {
+    return (
+        <p>My main goal: Learn React in-depth and from the ground up</p>
+    );
+}
+
+export default MainGoal;
---
+++ src/exercises/01-working-with-functions.js
@@ -0,0 +1,16 @@
+/*
+Write a new function that should be named combine and have the following characteristics:
+
+Accept three input values
+
+Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+Return the calculated result
+*/
+
+
+function combine(a, b, c) {
+    return a * b / c;
+}
+
+console.log("new value: " + combine(1, 2, 3));
